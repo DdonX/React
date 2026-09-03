e85c40e22bec9f3e60c523590a05374bb8eceb7d
DdonX <121351830+DdonX@users.noreply.github.com>
2026-09-03 12:28:34 +0000

    feat(added weather app) - added weather app with tailwind

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import Footer from './footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './About';
+import Weather from './Weather';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/weather" element={<Weather />} />
       </Routes>
       <Footer />
     </Router>
